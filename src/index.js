9837dacaffd299fd0c369cbbefdf1c21a7597ce8
Ivan <fenomen_ny@mail.ru>
2022-04-03 13:31:21 +0300

    методы полчения всех персонажей и конкретного по id

--- src/index.js
+++ src/index.js
@@ -6,11 +6,26 @@ import './style/style.scss';
 
 const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then(res => console.log(res));
+// получить всех персонажей
+// marvelService.getAllCharacters().then(res => console.log(res));
+
+// получить всех персонажей используя вложенность
+// marvelService.getAllCharacters().then(res => console.log(res.data.results));
+
+// получить всех персонажей, далее именно name у этих персонажей
+marvelService.getAllCharacters().then(
+	res => res.data.results.forEach(item => console.log(item.name)));
+
+	
+// получить по id статически
+// marvelService.getCharacter().then(res => console.log(res));
+
+// получить по id динамически
+// marvelService.getCharacter(1009610).then(res => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,9 @@
 class MarvelService {
+	// "скрытые" переменные
+	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
+	_apiKey = 'apikey=c1cd63c58fe773236e19227a127dc9e7';
+
+	// метод получения данных
   getResource = async (url) => {
     let res = await fetch(url);
 
@@ -9,11 +14,26 @@ class MarvelService {
     return await res.json();
   };
 
+	// метод получения всех персонажей
   getAllCharacters = () => {
     return this.getResource(
-      'https://gateway.marvel.com:443/v1/public/characters?apikey=c1cd63c58fe773236e19227a127dc9e7'
+			`${this._apiBase}characters?limit=9&offset=1241&${this._apiKey}`
     );
   };
+
+	// метод с пауком по id
+  getCharacter = () => {
+    return this.getResource(
+			`${this._apiBase}characters/1009610?${this._apiKey}`
+    );
+  };
+
+	// метод с динамическим id
+  // getCharacter = (id) => {
+  //   return this.getResource(
+	// 		`${this._apiBase}characters/${id}?${this._apiKey}`
+  //   );
+  // };
 }
 
 export default MarvelService;
